418138e6be6103fa71a0ecb80528d06625c3755b
pakorn <pakorn.worakarn@gmail.com>
2020-06-04 03:32:27 +0700

    update

--- src/Footer.js
+++ src/Footer.js
@@ -4,21 +4,26 @@ function Footer() {
     
     const [time, setTime] = useState(0)
 
+    //useEffect run after Render
     useEffect(() => {
 
         const interval = setInterval(() => {
             setTime(curretTime => curretTime + 1);
         }, 1000);
 
+        return () => clearInterval(interval);
+
     },[]);
 
     //ถ้าไม่มีการใส่ Argument เลย จะทำงานทุกครั้ง เมื่อข้อมูลมีการเปลี่ยนแปลงของ Component นั้น
     //ถ้าใส่เป็น Array ว่าง ([]) จะเป็นการทำงานเฉพาะตอน mount และ unmount
-    //ถ้าใส่ state ลงไปใน Argument ([time]) มันจะทำงาน ก็ต่อเมื่อค่าที่ระบุลงไปนั้นมีการเปลี่ยนแปลง อัพเดท)
+    //ถ้าใส่ state ลงไปใน Argument ([time]) มันจะทำงาน ก็ต่อเมื่อค่าที่ระบุลงไปนั้นมีการเปลี่ยนแปลง (อัพเดท)
 
+   console.log('render')
     return (
         <div>
             Footer Online Time {time}
+            <button type="button" onClick={() => setTime(curretTime => curretTime + 1) }>Add</button>
         </div>
     )
 }
