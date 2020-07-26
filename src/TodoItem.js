de91e7ab8a97747ba914edcc6e67989f9fca6beb
pakorn <pakorn.worakarn@gmail.com>
2020-07-27 00:00:37 +0700

    update commit

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,21 +1,30 @@
-import React, {useState} from "react";
+import React, {useState} from "react"
+import styled from 'styled-components'
+
+const Text = styled.span`
+  font-size: 18px;
+  color: ${props => (props.completed ? 'green' : '')}
+`
 
 const TodoItem = (props) => {
 
-  let { text } = props;
+  let { text } = props
 
-  const [status,setStatus] = useState(false);
+  const [status,setStatus] = useState(false)
 
   const toggle = () => {
-      setStatus(currentStatus => !currentStatus);
+      setStatus(currentStatus => !currentStatus)
   }
 
   return (
     <div>
       <input type="checkbox" value={status} onClick={toggle} />
-      {status ? 'done :' + text : text}
+      <Text completed={status}>
+        {status ? 'Completed :' + text : text}
+      </Text>
+      
     </div>
   );
 };
 
-export default TodoItem;
+export default TodoItem
